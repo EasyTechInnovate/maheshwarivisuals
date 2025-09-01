b9aa442ff0e64c87583a6167e250f75b016e00f3
Swarup Bhise <swarupbhise420@gmail.com>
2025-09-01 22:38:03 +0530

    feat : vite Configuration update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/admin',
+  server: {
+    host: true,
+    port: 8001,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
