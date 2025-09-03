3cccec3a8ef161986196b14734ab8b32d73debc1
piyush-github12 <piyushgithubaccouunt@gmail.com>
2025-09-04 01:53:58 +0530

    user Royalties Page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/dashboard/Dashboard'
 import UploadRelease from './pages/uploadRelease/UploadRelease'
 import CatalogPage from './pages/catalog/Catalog'
 import Analytics from './pages/analytics/Analytics'
+import Royalties from './pages/royalties/Royalties'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='upload-release' element={<UploadRelease/>}/>
           <Route path='catalog' element={<CatalogPage/>}/>
           <Route path='analytics' element={<Analytics/>}/>
+          <Route path='royalties' element={<Royalties/>}/>
         </Route>
       </Routes>
     </div>
