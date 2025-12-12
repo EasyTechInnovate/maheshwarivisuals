68d5c047ed9aa139033306aac511a3222d1445d3
Piyush Gayakwad <148153293+piyush-github12@users.noreply.github.com>
2025-12-12 12:39:28 +0530

    Update axios base URL configuration

--- admin/src/lib/GlobalApi.jsx
+++ admin/src/lib/GlobalApi.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "https://mv.easytechinnovate.site",
+  baseURL: import.meta.env.VITE_API_URL || 'https://api-mv.manishdashsharma.com',
+  // baseURL: "https://mv.easytechinnovate.site",
 });
 
 
